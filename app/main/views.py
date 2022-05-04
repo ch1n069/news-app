f94f9211d22947d9d424656a8b55c90b5d8f2685
Moringa School <brunolalpachino@gmail.com>
2022-05-04 10:40:12 +0300

    defined the get source for bbc news

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,19 @@
 from venv import main
 from flask import render_template
-from ..requests import get_source, get_articles, get_articles_from_source_selected, get_articles_depending_on_category_of_the_source
+from ..request import get_source, get_articles, get_articles_from_source_selected, get_articles_depending_on_category_of_the_source
 from . import main 
 
 
 
 @main.route('/')
 def index():
+
+
+    sources = get_source()
+    # get articles from bbc-news
+    bbc_news = get_articles_from_source_selected('bbc-news', '8')
+    # get articles from al-jazeera-english
+
 
 
     return render_template()
