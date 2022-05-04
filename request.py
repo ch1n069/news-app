f865fccb56f0fbe680e5d501e296f0f39aa9ad33
Moringa School <brunolalpachino@gmail.com>
2022-05-04 08:41:03 +0300

    created function that processes sources result to transform them into objects

--- request.py
+++ request.py
@@ -12,3 +12,11 @@ base_url_source = None
 base_url_articles = None
 
 
+def configure_request(app):
+    global api_key, base_url_source, base_url_articles
+    base_url_source = app.config['NEWS_API_SOURCE_URL']
+    base_url_articles = app.config['NEWS_API_ARTICLES_URL']
+    api_key = app.config['NEWS_API_KEY']
+
+
+
