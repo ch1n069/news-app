fc8139fc4b8f88c442ce43ac77a71c86b14b3f0b
Moringa School <brunolalpachino@gmail.com>
2022-05-04 08:38:43 +0300

    prod files

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,18 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()###activate the boostrap extensions
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+
+    app.config.from_object(config_map[config_name])
+    config_map[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+
 
 app = Flask(__name__)
 
