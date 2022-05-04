33e5cc71e04f3ba42dcd97d5a426cdd23b2e7342
Moringa School <brunolalpachino@gmail.com>
2022-05-04 08:42:23 +0300

    added the same return function for the articles

--- request.py
+++ request.py
@@ -20,3 +20,46 @@ def configure_request(app):
 
 
 
+def process_results_sources(source_list):
+    '''
+    Function that processes the source list result and transform them to a list of Objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+
+        source_object = Sources(
+            id, name, description, url, category, language)
+        source_results.append(source_object)
+
+    return source_results
+
+def process_results_articles(articles_list):
+    '''
+    Function that processes the articles list result and transform them to a list of Objects
+    '''
+    articles_results = []
+    for article_item in articles_list:
+        author = article_item.get('author')
+        title = article_item.get('title')
+        description = article_item.get('description')
+        url = article_item.get('url')
+        urlToImage = article_item.get('urlToImage')
+        publishedAt = article_item.get('publishedAt')
+        content = article_item.get('content')
+
+        if urlToImage:
+            articles_object = Articles(
+                author, title, description, url, urlToImage, publishedAt, content)
+            articles_results.append(articles_object)
+
+    return articles_results
+
+
+
+
